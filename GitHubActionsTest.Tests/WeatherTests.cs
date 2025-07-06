dffb007e3f0b5baa7819171226236790b26e0011
Kieran Chandler <bwoodyouth_14@hotmail.co.uk>
2025-07-06 01:15:34 +0100

    Created API project and test project covering one endpoint of 2 endpoints

---
+++ GitHubActionsTest.Tests/WeatherTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Shouldly;
+
+namespace GitHubActionsTest.Tests;
+
+public class WeatherTests
+{
+    [Fact]
+    public async Task CanGetForecast()
+    {
+        using var client = new WebApplicationFactory<Program>()
+            .CreateClient();
+
+        var response = await client.GetAsync("WeatherForecast");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+}
